2c5b189035b33785bf02db09b448b0a946125525
Rodolfo <rudolfrce@gmail.com>
2022-02-02 17:21:25 -0700

    First commit, calcultator

---
+++ main.js
@@ -0,0 +1,25 @@
+// basic calculator functions
+const numbers = new Array();
+const add = (a, b) => {
+  return a + b;
+};
+const substract = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+const divide = (a, b) => {
+  return a / b;
+};
+const operator = (operation, numbers) => {
+  if (operation === "+") {
+    return add(...numbers);
+  } else if (operation === "-") {
+    return substract(...numbers);
+  } else if (operation === "x") {
+    return multiply(...numbers);
+  } else if (operation === "/") {
+    return divide(...numbers);
+  }
+};
