a2e08fe79db10fbcee2b2d259761bcd48e25b362
Rodolfo <rudolfrce@gmail.com>
2022-02-08 13:18:14 -0700

    First basic calculator

--- main.js
+++ main.js
@@ -26,8 +26,33 @@ const operator = (operation, numbers) => {
 
 // Dom interaction
 const buttons = document.querySelectorAll("button");
+const displayInputs = document.querySelector(".display-values");
+const displayResult = document.querySelector(".display-result");
+let stringVars = new String();
+let arrayVars = new Array();
+
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target);
+    const operations = e.target.dataset.operation;
+    if (operations === "=") {
+      let operationOption = arrayVars[1];
+      arrayVars.splice(1, 1);
+      let arrayNumbers = arrayVars.map((stringNumber) => {
+        return Number(stringNumber);
+      });
+      console.log(arrayNumbers);
+      displayInputs.innerHTML = 0;
+      displayResult.innerHTML = operator(operationOption, arrayNumbers);
+      arrayVars = operator(operationOption, arrayNumbers);
+      console.log(arrayVars);
+    } else if (button.className === "operation" && operations !== "=") {
+      stringVars += " " + operations + " ";
+      arrayVars = stringVars.split(/ /g);
+      displayInputs.innerHTML = stringVars.replace(/ /g, "");
+    } else {
+      stringVars += operations;
+      arrayVars = stringVars.split(/ /g);
+      displayInputs.innerHTML = stringVars.replace(/ /g, "");
+    }
   });
 });
