1727d99b92fe731874f764ff9029093ef6243d6c
Rodolfo <rudolfrce@gmail.com>
2022-02-08 18:12:10 -0700

    Solve some problems with negative numbers

--- main.js
+++ main.js
@@ -31,6 +31,30 @@ const displayResult = document.querySelector(".display-result");
 let stringVars = new String();
 let arrayVars = new Array();
 
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     const operations = e.target.dataset.operation;
+//     if (operations === "=") {
+//       let operationOption = arrayVars[1];
+//       arrayVars.splice(1, 1);
+//       let arrayNumbers = arrayVars.map((stringNumber) => {
+//         return Number(stringNumber);
+//       });
+//       displayInputs.innerHTML = operator(operationOption, arrayNumbers);
+//       displayResult.innerHTML = operator(operationOption, arrayNumbers);
+//       stringVars = String(operator(operationOption, arrayNumbers));
+//     } else if (button.className === "operation" && operations !== "=") {
+//       stringVars += " " + operations + " ";
+//       arrayVars = stringVars.split(/ /g);
+//       displayInputs.innerHTML = stringVars.replace(/ /g, "");
+//     } else {
+//       stringVars += operations;
+//       arrayVars = stringVars.split(/ /g);
+//       displayInputs.innerHTML = stringVars.replace(/ /g, "");
+//     }
+//   });
+// });
+
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const operations = e.target.dataset.operation;
@@ -40,11 +64,8 @@ buttons.forEach((button) => {
       let arrayNumbers = arrayVars.map((stringNumber) => {
         return Number(stringNumber);
       });
-      console.log(arrayNumbers);
-      displayInputs.innerHTML = 0;
-      displayResult.innerHTML = operator(operationOption, arrayNumbers);
-      arrayVars = operator(operationOption, arrayNumbers);
-      console.log(arrayVars);
+      displayInputs.innerHTML = operator(operationOption, arrayNumbers);
+      stringVars = String(operator(operationOption, arrayNumbers));
     } else if (button.className === "operation" && operations !== "=") {
       stringVars += " " + operations + " ";
       arrayVars = stringVars.split(/ /g);
@@ -53,6 +74,10 @@ buttons.forEach((button) => {
       stringVars += operations;
       arrayVars = stringVars.split(/ /g);
       displayInputs.innerHTML = stringVars.replace(/ /g, "");
+    }
+    if (arrayVars[1] === "-") {
+      arrayVars[2] = -arrayVars[2];
+      arrayVars.splice(0, 2);
     }
   });
 });
