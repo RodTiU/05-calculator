dbafd074195c5858e05723bb65c4e06b4e7b9c9d
Rodolfo <rudolfrce@gmail.com>
2022-02-10 16:40:45 -0700

    Refactor calculator for better experience, first commit

--- main.js
+++ main.js
@@ -26,58 +26,36 @@ const operator = (operation, numbers) => {
 
 // Dom interaction
 const buttons = document.querySelectorAll("button");
-const displayInputs = document.querySelector(".display-values");
-const displayResult = document.querySelector(".display-result");
-let stringVars = new String();
-let arrayVars = new Array();
+const displayResult = document.querySelector(".display-values");
 
-// buttons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     const operations = e.target.dataset.operation;
-//     if (operations === "=") {
-//       let operationOption = arrayVars[1];
-//       arrayVars.splice(1, 1);
-//       let arrayNumbers = arrayVars.map((stringNumber) => {
-//         return Number(stringNumber);
-//       });
-//       displayInputs.innerHTML = operator(operationOption, arrayNumbers);
-//       displayResult.innerHTML = operator(operationOption, arrayNumbers);
-//       stringVars = String(operator(operationOption, arrayNumbers));
-//     } else if (button.className === "operation" && operations !== "=") {
-//       stringVars += " " + operations + " ";
-//       arrayVars = stringVars.split(/ /g);
-//       displayInputs.innerHTML = stringVars.replace(/ /g, "");
-//     } else {
-//       stringVars += operations;
-//       arrayVars = stringVars.split(/ /g);
-//       displayInputs.innerHTML = stringVars.replace(/ /g, "");
-//     }
-//   });
-// });
-
+let stringValues = "";
+let matchNumbersStringArray;
+let matchOperationsArray = [];
+let matchNumbersArray;
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const operations = e.target.dataset.operation;
-    if (operations === "=") {
-      let operationOption = arrayVars[1];
-      arrayVars.splice(1, 1);
-      let arrayNumbers = arrayVars.map((stringNumber) => {
-        return Number(stringNumber);
+    const value = e.target.dataset.operation;
+    if (value !== "=" && value !== "-") {
+      stringValues += value;
+    } else if (value === "-") {
+      stringValues += "+" + value;
+    } else if (value === "=" && matchNumbersStringArray !== null) {
+      let operation = matchOperationsArray[0];
+      let result = operator(operation, matchNumbersArray).toFixed(2);
+      displayResult.innerHTML = result;
+      stringValues = `${result}`;
+      matchOperationsArray = [];
+      return;
+    }
+    matchNumbersStringArray = stringValues.match(
+      /\-?\d+\.\d+|\-?\d+|\-?\.\d+/g
+    );
+    matchOperationsArray = stringValues.match(/\+|x|\//g);
+    if (matchNumbersStringArray !== null) {
+      matchNumbersArray = matchNumbersStringArray.map((numberString) => {
+        return Number(numberString);
       });
-      displayInputs.innerHTML = operator(operationOption, arrayNumbers);
-      stringVars = String(operator(operationOption, arrayNumbers));
-    } else if (button.className === "operation" && operations !== "=") {
-      stringVars += " " + operations + " ";
-      arrayVars = stringVars.split(/ /g);
-      displayInputs.innerHTML = stringVars.replace(/ /g, "");
-    } else {
-      stringVars += operations;
-      arrayVars = stringVars.split(/ /g);
-      displayInputs.innerHTML = stringVars.replace(/ /g, "");
     }
-    if (arrayVars[1] === "-") {
-      arrayVars[2] = -arrayVars[2];
-      arrayVars.splice(0, 2);
-    }
+    displayResult.innerHTML = stringValues.replace(/\+\-/g, "-");
   });
 });
